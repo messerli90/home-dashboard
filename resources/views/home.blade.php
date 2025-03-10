15d6af3df1c873b128b5bc02f79d1f900260564d
Michael Messerli <michaelamesserli@gmail.com>
2025-03-10 16:03:02 -0600

    Init project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+    <h1>Home</h1>
+    <p>Welcome to our website!</p>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ $page_title ?? 'Dashboard' }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=abeezee:400" rel="stylesheet" />
+
+        <!-- Styles / Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
